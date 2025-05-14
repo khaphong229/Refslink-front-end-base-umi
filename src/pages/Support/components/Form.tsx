47a1d5106d35261a5660f16ebb7d7062b8e4c544
tudgvn23 <tuduongvan2305@gmail.com>
2025-05-14 17:17:02 +0700

    feat: added form support

--- src/pages/Support/components/Form.tsx
+++ src/pages/Support/components/Form.tsx
@@ -1,39 +1,75 @@
-import React from "react";
-import {Card, Form,Input, message } from "antd";
+import React from 'react';
+import { Card, Form, Input, message, Button } from 'antd';
 
-const {TextArea} = Input;
+const { TextArea } = Input;
 
 const SupportForm: React.FC = () => {
-    const [form] = Form.useForm();
-    
-    const handleSubmit = (values:any) =>{
-        // call api
-        message.success("Gửi yêu cầu hỗ trợ thành công")
-        form.resetFields()
+	const [form] = Form.useForm();
 
+	const handleSubmit = (values: any) => {
+		// call api
+		message.success('Gửi yêu cầu hỗ trợ thành công');
+		form.resetFields();
+	};
 
-    }
+	return (
+		<Card title='Gửi yêu cầu hỗ trợ'>
+			<Form form={form} layout='vertical' onFinish={handleSubmit}>
+				<Form.Item name='full_name' label='Họ và tên' rules={[{ required: true, message: 'Vui lòng nhập họ và tên' }]}>
+					<Input placeholder='Nguyễn Văn A' />
+				</Form.Item>
 
-    return (
-        <Card
-        title="Gửi yêu cầu hỗ trợ"
+				<Form.Item
+					name='email'
+					label='Email'
+					rules={[
+						{
+							required: true,
+							message: 'Vui lòng nhập email',
+						},
+						{
+							type: 'email',
+							message: 'Email không đúng định dạng',
+						},
+					]}
+				>
+					<Input placeholder='nguyenvana@gmail.com'></Input>
+				</Form.Item>
 
-        >
-            <Form
-            form={form}
-            layout="vertical"
-            onFinish={handleSubmit}
-            >
-                <Form.Item
-                label="full_name"
-                
-                >
+				<Form.Item
+					label='Chủ đề '
+					name='subject'
+					rules={[
+						{
+							required: true,
+							message: 'Vui lòng nhập tiêu đề vấn đề ',
+						},
+					]}
+				>
+					<Input placeholder='Ví dụ: Không thể rút gọn link' />
+				</Form.Item>
 
-                </Form.Item>
+				<Form.Item
+					label='Mô tả chi tiết'
+					name='description'
+					rules={[
+						{
+							required: true,
+							message: 'Vui lòng nhập mô tả chi tiết',
+						},
+					]}
+				>
+					<TextArea rows={4} placeholder='Mô tả chi tiết vấn đề của bạn' />
+				</Form.Item>
 
+				<Form.Item>
+					<Button type='primary' htmlType='submit'>
+						Gửi yêu cầu
+					</Button>
+				</Form.Item>
+			</Form>
+		</Card>
+	);
+};
 
-            </Form>
-        </Card>
-
-    )
-}
+export default SupportForm;
---
+++ src/pages/Support/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SupportForm from "./components/Form";
+
+const SupportPage:React.FC = () =>{
+    return (
+        <div>
+            <SupportForm/>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default SupportPage
