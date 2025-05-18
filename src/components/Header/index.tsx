75560f6db48c5c67e2768f53481d1cd8e7ece9fc
khaphong229 <khaphong217@gmail.com>
2025-05-18 19:26:33 +0700

    fix(Header): fix scroll to section, dont use library

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,24 +2,31 @@ import { useHistory } from 'react-router-dom';
 import { MenuOutlined } from '@ant-design/icons';
 import { Button, Dropdown, Menu } from 'antd';
 import './style.less';
-import { Link } from 'react-scroll';
 
 const Header = () => {
 	const history = useHistory();
+
+	// Hàm cuộn mượt đến section
+	const scrollToSection = (id: string) => {
+		const el = document.getElementById(id);
+		if (el) {
+			el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		}
+	};
 
 	const menu = (
 		<Menu>
 			<Menu.Item onClick={() => history.push('/')}>Trang chủ</Menu.Item>
 			<Menu.Item key='introduce'>
-				<Link to='introduce' smooth={true} duration={500} offset={-64}>
+				<Button type='text' onClick={() => scrollToSection('introduce')}>
 					Giới thiệu
-				</Link>
+				</Button>
 			</Menu.Item>
-			<Menu.Item key="connect">
-        <Link to="connect" smooth={true} duration={500} offset={-64}>
-          Liên hệ
-        </Link>
-      </Menu.Item>
+			<Menu.Item key='connect'>
+				<Button type='text' onClick={() => scrollToSection('connect')}>
+					Liên hệ
+				</Button>
+			</Menu.Item>
 			<Menu.Divider />
 			<Menu.Item onClick={() => history.push('/user/login')}>Đăng nhập</Menu.Item>
 			<Menu.Item key='mode'>Chế độ sáng/tối</Menu.Item> {/* Có thể gắn toggle ở đây */}
@@ -33,28 +40,26 @@ const Header = () => {
 					<span>Ref</span>
 					<span className='text-gradient'>slink</span>
 				</div>
+
 				<div className='nav-links'>
-					<Button className='rounded-btn' type='link'>
+					<Button className='rounded-btn' type='link' onClick={() => history.push('/')}>
 						Trang chủ
 					</Button>
-					<Link to='introduce' smooth={true} duration={500} offset={-64}>
-						<Button className='rounded-btn' type='link'>
-							Giới thiệu
-						</Button>
-					</Link>
-					<Link to='connect' smooth={true} duration={500} offset={-64}>
-						<Button className='rounded-btn' type='link'>
-							Liên hệ
-						</Button>
-					</Link>
+					<Button className='rounded-btn' type='link' onClick={() => scrollToSection('introduce')}>
+						Giới thiệu
+					</Button>
+					<Button className='rounded-btn' type='link' onClick={() => scrollToSection('connect')}>
+						Liên hệ
+					</Button>
 				</div>
+
 				<div className='auth-mode'>
 					<Button className='rounded-btn' type='primary' onClick={() => history.push('/user/login')}>
 						Đăng ký | Đăng nhập
 					</Button>
-					{/* Nếu bạn đã có component ModeToggle, có thể nhúng vào đây */}
 					{/* <ModeToggle /> */}
 				</div>
+
 				<div className='menu-mobile'>
 					<Dropdown overlay={menu} placement='bottomRight' trigger={['click']}>
 						<Button className='rounded-btn' icon={<MenuOutlined />} />
